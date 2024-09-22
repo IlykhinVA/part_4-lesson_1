2587652d97dac5bbe8911f4164a8831c4edea0f6
IlykhinVA <priest49175@gmail.com>
2024-09-22 12:29:39 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as evklid
+from true_math import divide as lobach
+
+result1 = evklid(69, 3)
+result2 = evklid(3, 0)
+result3 = lobach(49, 7)
+result4 = lobach(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
